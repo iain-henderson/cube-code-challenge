9eed9d177f8db0916ce3dc3ebe162743c31c02fb
IainJamesHenderson <i.henderson@terrawi.com>
2021-12-03 13:02:01 -0500

    initial commit

---
+++ project/dimensions/challenge.py
@@ -0,0 +1,12 @@
+from .models import Company, Dimension
+
+
+def list_children(dimension_id):
+    """ List a dimension and all its children in a nested hierarchy. """
+    # TODO - See README for expected output
+    pass
+
+def list_hierarchy(company_id):
+    """ List the complete nested hierarchy for a company. """
+    # TODO - See README for expected output
+    pass
